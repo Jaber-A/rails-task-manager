53fd3266f28ef43c066df9faf3af5402828d2d9e
Jaber AlAdawi <jaberadawi@gmail.com>
2020-08-12 10:42:33 +0200

    first Model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,10 @@ class TasksController < ApplicationController
     def index
       @tasks = Task.all
     end
+    
+    def new
+      @task = Task.new
+    end
 
     def show
       @task = Task.find(params[:id])
